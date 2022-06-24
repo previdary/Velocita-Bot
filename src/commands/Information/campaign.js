27373f65b5ec7918dbd44626cf566b70407ecb23
luca-paolo <mitu@vite.global>
2022-06-24 16:41:27 -0500

    update desc

--- src/commands/Information/campaign.js
+++ src/commands/Information/campaign.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder} = require('@discordjs/builders')
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('campaign')
-        .setDescription('Say hello to the bot'),
+        .setDescription('Displays Prevs Campaign'),
     async execute(interaction) {
         await interaction.reply('Vote Prev for earl today!')
     }
